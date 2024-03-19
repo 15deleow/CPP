2f827d1cb6d83a61f37b6c7b6fbab7dfa7509863
William Deleon <williamdeleon87@yahoo.com>
2024-03-19 15:32:40 -0700

    Fixes misspelling of Utilities

---
+++ bankManagementSystemServer/Utilities/common.h
@@ -0,0 +1,15 @@
+#include <time.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+struct account_data {
+    std::string name;
+    std::string accountNumber;
+    int age;
+    std::string address;
+    int accountType;
+    std::string phoneNumber;
+    std::string dob;
+    std::string date;
+};
